4d12130493d18e2b9d1e65e31af7dd9ed64c30fe
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-06-18 02:31:57 +0545

    Unistaller

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,7 @@
  *
  * @author   ThemeGrill
  * @category Core
+ * @package  Importer/Unistaller
  * @version  1.4.0
  */
 
@@ -13,6 +14,14 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-// Delete options.
-delete_option( 'themegrill_demo_imported_id' );
-delete_option( 'themegrill_demo_imported_notice_dismiss' );
+global $wpdb;
+
+/*
+ * Only remove ALL demo importer data if TG_DEMO_IMPORTER_REMOVE_ALL_DATA constant is set to true in user's
+ * wp-config.php. This is to prevent data loss when deleting the plugin from the backend
+ * and to ensure only the site owner can perform this action.
+ */
+if ( defined( 'TG_DEMO_IMPORTER_REMOVE_ALL_DATA' ) && true === TG_DEMO_IMPORTER_REMOVE_ALL_DATA ) {
+	// Delete options.
+	$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE 'themegrill_demo_imported\_%';" );
+}
