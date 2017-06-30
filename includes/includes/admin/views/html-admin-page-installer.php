5aa7b23b51c162e7e6ab5eff621e90f19895c6a9
Sanjeev Shrestha <sethstha@gmail.com>
2017-06-30 16:41:40 +0545

    Fixed layout issue and responsive youtube video

--- includes/includes/admin/views/html-admin-page-installer.php
+++ includes/includes/admin/views/html-admin-page-installer.php
@@ -85,7 +85,9 @@ $demo_filter_links = apply_filters( 'themegrill_demo_importer_filter_links_array
 				</div>
 			</div>
 			<div class="welcome-panel-iframe-video">
-				<iframe width="560" height="315" src="<?php echo esc_url( 'https://www.youtube.com/embed/kQlvX0FlSmo?rel=0&amp;showinfo=0' ); ?>" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+				<div class="welcome-panel-iframe-video-inner">
+					<iframe width="560" height="315" src="<?php echo esc_url( 'https://www.youtube.com/embed/kQlvX0FlSmo?rel=0&amp;showinfo=0' ); ?>" allowtransparency="true" frameborder="0" scrolling="no" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+				</div>
 			</div>
 		</div>
 	<?php else : ?>
