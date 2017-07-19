2296bd44f49b2e91a358055b1cd6c5772fbfa4fd
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-07-19 23:34:54 +0545

    Better logic to control plugin actions

--- includes/admin/views/html-admin-page-installer.php
+++ includes/admin/views/html-admin-page-installer.php
@@ -243,8 +243,8 @@ $demo_filter_links = apply_filters( 'themegrill_demo_importer_filter_links_array
 						<tfoot>
 							<tr>
 								<th scope="col" class="manage-column plugin-actions<# if ( ! data.pluginActions['install'] ) { #> installed<# } #>" colspan="4">
-									<a href="#" class="button button-secondary plugins-install"<# if ( ! data.pluginActions['install'] ) { #> disabled<# } #>><?php _e( 'Install Plugins', 'themegrill-demo-importer' ); ?></a>
-									<input type="submit" name="bulk_action" id="bulk_action" class="button button-primary plugins-activate" value="<?php esc_attr_e( __( 'Activate Plugins', 'themegrill-demo-importer' ) ); ?>"<# if ( ! data.pluginActions['activate'] ) { #> disabled<# } #>>
+									<a href="#" class="button button-primary plugins-install<# if ( ! data.pluginActions['install'] ) { #> disabled<# } #>"><?php _e( 'Install Plugins', 'themegrill-demo-importer' ); ?></a>
+									<input type="submit" name="bulk_action" id="bulk_action" class="button button-secondary plugins-activate" value="<?php esc_attr_e( __( 'Activate Plugins', 'themegrill-demo-importer' ) ); ?>"<# if ( ! data.pluginActions['activate'] ) { #> disabled<# } #>>
 								</th>
 							</tr>
 						</tfoot>
