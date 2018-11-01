e5984fdd85193880bc813c525aa43885e6c5aa6d
Bishal Napit <mebishalnapit@gmail.com>
2018-11-01 14:55:52 +0545

    Support Zakra theme

--- includes/class-themegrill-demo-importer.php
+++ includes/class-themegrill-demo-importer.php
@@ -120,7 +120,7 @@ final class ThemeGrill_Demo_Importer {
 	 * @return array
 	 */
 	private function get_core_supported_themes() {
-		$core_themes = array( 'spacious', 'colormag', 'flash', 'estore', 'ample', 'accelerate', 'colornews', 'foodhunt', 'fitclub', 'radiate', 'freedom', 'himalayas', 'esteem', 'envince', 'suffice', 'explore', 'masonic', 'cenote', 'modular' );
+		$core_themes = array( 'spacious', 'colormag', 'flash', 'estore', 'ample', 'accelerate', 'colornews', 'foodhunt', 'fitclub', 'radiate', 'freedom', 'himalayas', 'esteem', 'envince', 'suffice', 'explore', 'masonic', 'cenote', 'zakra' );
 
 		// Check for official core themes pro version.
 		$pro_themes = array_diff( $core_themes, array( 'explore', 'masonic' ) );
