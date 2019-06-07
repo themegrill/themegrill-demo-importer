3b815c03786269b291fd8e59f1ed11310cd55082
Bishal Napit <mebishalnapit@gmail.com>
2019-06-07 16:51:59 +0545

    Enqueue jQuery Confirm library file

--- includes/class-demo-importer.php
+++ includes/class-demo-importer.php
@@ -153,6 +153,7 @@ class TG_Demo_Importer {
 
 		// Register admin styles.
 		wp_register_style( 'tg-demo-importer', $assets_path . 'css/demo-importer.css', array(), TGDM_VERSION );
+		wp_register_style( 'jquery-confirm', $assets_path . 'css/jquery-confirm/jquery-confirm.css', array(), TGDM_VERSION );
 
 		// Add RTL support for admin styles.
 		wp_style_add_data( 'tg-demo-importer', 'rtl', 'replace' );
@@ -161,11 +162,14 @@ class TG_Demo_Importer {
 		wp_register_script( 'jquery-tiptip', $assets_path . 'js/jquery-tiptip/jquery.tipTip' . $suffix . '.js', array( 'jquery' ), '1.3', true );
 		wp_register_script( 'tg-demo-updates', $assets_path . 'js/admin/demo-updates' . $suffix . '.js', array( 'jquery', 'updates' ), TGDM_VERSION, true );
 		wp_register_script( 'tg-demo-importer', $assets_path . 'js/admin/demo-importer' . $suffix . '.js', array( 'jquery', 'jquery-tiptip', 'wp-backbone', 'wp-a11y', 'tg-demo-updates' ), TGDM_VERSION, true );
+		wp_register_script( 'jquery-confirm', $assets_path . 'js/jquery-confirm/jquery-confirm' . $suffix . '.js', array( 'jquery' ), TGDM_VERSION, true );
 
 		// Demo Importer appearance page.
 		if ( 'appearance_page_demo-importer' === $screen_id ) {
 			wp_enqueue_style( 'tg-demo-importer' );
 			wp_enqueue_script( 'tg-demo-importer' );
+			wp_enqueue_style( 'jquery-confirm' );
+			wp_enqueue_script( 'jquery-confirm' );
 			wp_localize_script(
 				'tg-demo-updates',
 				'_demoUpdatesSettings',
