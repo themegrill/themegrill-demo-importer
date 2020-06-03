b4352ddf0db5cc0d77f8a27bb23c65a9527b76e7
Bishal Napit <mebishalnapit@gmail.com>
2020-06-03 13:29:46 +0545

    Tweak - Modify display of review notice

--- includes/admin/class-plugin-review-notice.php
+++ includes/admin/class-plugin-review-notice.php
@@ -46,6 +46,9 @@ class TG_Demo_Importer_Review_Notice {
 		$ignored_notice           = get_user_meta( $user_id, 'tg_demo_importer_plugin_review_notice', true );
 		$ignored_notice_partially = get_user_meta( $user_id, 'nag_tg_demo_importer_plugin_review_notice_partially', true );
 
+		// Check for if demo is already imported.
+		$demo_imported = get_option( 'themegrill_demo_importer_activated_id' );
+
 		/**
 		 * Return from notice display if:
 		 *
@@ -53,14 +56,20 @@ class TG_Demo_Importer_Review_Notice {
 		 * 2. If the user has ignored the message partially for 15 days.
 		 * 3. Dismiss always if clicked on 'I Already Did' button.
 		 */
-		if ( ( get_option( 'tg_demo_importer_plugin_installed_time' ) > strtotime( '-15 day' ) ) || ( $ignored_notice_partially > strtotime( '-15 day' ) ) || ( $ignored_notice ) ) {
-			return;
+		if ( ! $demo_imported ) {
+			if ( ( get_option( 'tg_demo_importer_plugin_installed_time' ) > strtotime( '-15 day' ) ) || ( $ignored_notice_partially > strtotime( '-15 day' ) ) || ( $ignored_notice ) ) {
+				return;
+			}
+		} else {
+			if ( ( $ignored_notice_partially > strtotime( '-15 day' ) ) || ( $ignored_notice ) ) {
+				return;
+			}
 		}
 		?>
 		<div class="notice notice-success tg-demo-importer-notice theme-review-notice" style="position:relative;">
 			<p>
 				<?php
-				printf(
+				$message = sprintf(
 					/* Translators: %1$s current user display name. */
 					esc_html__(
 						'Howdy, %1$s! It seems that you have been using this plugin for more than 15 days. We hope you are happy with everything that the plugin has to offer. If you can spare a minute, please help us by leaving a 5-star review on WordPress.org.  By spreading the love, we can continue to develop new amazing features in the future, for free!',
@@ -68,6 +77,19 @@ class TG_Demo_Importer_Review_Notice {
 					),
 					'<strong>' . esc_html( $current_user->display_name ) . '</strong>'
 				);
+
+				if ( $demo_imported ) {
+					$message = sprintf(
+						/* Translators: %1$s current user display name. */
+						esc_html__(
+							'Howdy, %1$s! It seems that you have imported the theme demo in your site. We hope that you are happy with it and if you can spare a minute, please help us by leaving a 5-star review on WordPress.org.',
+							'themegrill-demo-importer'
+						),
+						'<strong>' . esc_html( $current_user->display_name ) . '</strong>'
+					);
+				}
+
+				echo $message;
 				?>
 			</p>
 
