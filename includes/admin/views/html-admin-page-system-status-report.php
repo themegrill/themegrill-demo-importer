a857dfa9c9f8b87e8efb62d49c73f4dc811fa461
Bishal Napit <mebishalnapit@gmail.com>
2020-08-03 09:09:30 +0545

    Tweak - Get additional PHP server informations

--- includes/admin/views/html-admin-page-system-status-report.php
+++ includes/admin/views/html-admin-page-system-status-report.php
@@ -22,23 +22,53 @@ global $wpdb;
 		</thead>
 		<tbody>
 		<tr>
-			<td><?php esc_html_e( 'Operating System', 'themegrill-demo-importer' ); ?></td>
+			<td><?php esc_html_e( 'Operating System:', 'themegrill-demo-importer' ); ?></td>
 			<td><?php echo esc_html( PHP_OS ); ?></td>
 			<td></td>
 		</tr>
 		<tr>
-			<td><?php esc_html_e( 'Server', 'themegrill-demo-importer' ); ?></td>
+			<td><?php esc_html_e( 'Serve:r', 'themegrill-demo-importer' ); ?></td>
 			<td><?php echo esc_html( wp_unslash( $_SERVER['SERVER_SOFTWARE'] ) ); ?></td>
 			<td></td>
 		</tr>
 		<tr>
-			<td><?php esc_html_e( 'MySQL Version', 'themegrill-demo-importer' ); ?></td>
+			<td><?php esc_html_e( 'MySQL Version:', 'themegrill-demo-importer' ); ?></td>
 			<td><?php echo esc_html( $wpdb->get_var( 'SELECT VERSION()' ) ); ?></td>
 			<td></td>
 		</tr>
 		<tr>
-			<td><?php esc_html_e( 'PHP Version', 'themegrill-demo-importer' ); ?></td>
+			<td><?php esc_html_e( 'PHP Version:', 'themegrill-demo-importer' ); ?></td>
 			<td><?php echo esc_html( PHP_VERSION ); ?></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td><?php esc_html_e( 'PHP Max Execution Time:', 'themegrill-demo-importer' ); ?></td>
+			<td><?php echo esc_html( ini_get( 'max_execution_time' ) ); ?></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td><?php esc_html_e( 'PHP Max Upload Size:', 'themegrill-demo-importer' ); ?></td>
+			<td><?php echo esc_html( ini_get( 'upload_max_filesize' ) ); ?></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td><?php esc_html_e( 'PHP Post Max Size:', 'themegrill-demo-importer' ); ?></td>
+			<td><?php echo esc_html( ini_get( 'post_max_size' ) ); ?></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td><?php esc_html_e( 'PHP Max Input Vars:', 'themegrill-demo-importer' ); ?></td>
+			<td><?php echo esc_html( ini_get( 'max_input_vars' ) ); ?></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td><?php esc_html_e( 'PHP Memory Limit:', 'themegrill-demo-importer' ); ?></td>
+			<td><?php echo esc_html( ini_get( 'memory_limit' ) ); ?></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td><?php esc_html_e( 'GD Installed:', 'themegrill-demo-importer' ); ?></td>
+			<td><?php echo esc_html( extension_loaded( 'gd' ) ? __( 'Yes', 'themegrill-demo-importer' ) : __( 'No', 'themegrill-demo-importer' ) ); ?></td>
 			<td></td>
 		</tr>
 		</tbody>
