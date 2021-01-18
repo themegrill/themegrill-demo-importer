6ef6c3d76f44c99c9cb4c5aacdb12fc2d1be0d8c
Bishal Napit <mebishalnapit@gmail.com>
2021-01-18 07:42:31 +0545

    Remove - Unrequired plugin review data sets removal in plugin deactivation

--- includes/class-demo-importer-deactivator.php
+++ includes/class-demo-importer-deactivator.php
@@ -19,36 +19,8 @@ class TG_Demo_Importer_Deactivator {
 	 */
 	public static function deactivate() {
 
-		// Delete the `Plugin Review` data sets.
-		self::plugin_review_notice();
-
 		// Delete the `Plugin Deactivate` data sets.
 		self::plugin_deactivate_notice();
-
-	}
-
-	/**
-	 * Delete the options set for `Plugin Review` admin notice.
-	 */
-	public static function plugin_review_notice() {
-
-		$get_all_users = get_users();
-
-		// Delete user meta data for theme review notice.
-		foreach ( $get_all_users as $user ) {
-			$ignored_notice           = get_user_meta( $user->ID, 'tg_demo_importer_plugin_review_notice', true );
-			$ignored_notice_partially = get_user_meta( $user->ID, 'nag_tg_demo_importer_plugin_review_notice_partially', true );
-
-			// Delete permanent notice remove data.
-			if ( $ignored_notice ) {
-				delete_user_meta( $user->ID, 'tg_demo_importer_plugin_review_notice' );
-			}
-
-			// Delete partial notice remove data.
-			if ( $ignored_notice_partially ) {
-				delete_user_meta( $user->ID, 'nag_tg_demo_importer_plugin_review_notice_partially' );
-			}
-		}
 
 	}
 
