6a6e290dec46c578535f486c5521e835b12d6731
Jenisha Munikar <znisamunikar@gmail.com>
2025-09-11 12:53:43 +0545

    added plugin description

--- resources/onboarding/lib/plugins.ts
+++ resources/onboarding/lib/plugins.ts
@@ -4,4 +4,8 @@ export const pluginsDetails: Record<string, string> = {
 		'Display your content in eye-catching magazine-style layouts.',
 	'blockart-blocks/blockart.php': 'Build beautiful pages using a simple drag-and-drop editor.',
 	'woocommerce/woocommerce.php': 'Sell products online and accept secure payments.',
+	'companion-elementor/companion-elementor.php':
+		'Extend Elementor with advanced widgets and design elements.',
+	'learning-management-system/lms.php':
+		'Create and sell online courses with quizzes and certificates.let visitors reach you through ',
 };
