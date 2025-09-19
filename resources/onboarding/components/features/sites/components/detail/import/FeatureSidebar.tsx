af4da9d092c97b2987aebe50932d898b4454da3f
Jenisha Munikar <znisamunikar@gmail.com>
2025-09-19 14:33:53 +0545

    plugin disabled ui changes

--- resources/onboarding/components/features/sites/components/detail/import/FeatureSidebar.tsx
+++ resources/onboarding/components/features/sites/components/detail/import/FeatureSidebar.tsx
@@ -1,11 +1,10 @@
-import { useRouter } from '@tanstack/react-router';
 import { __ } from '@wordpress/i18n';
-import { ArrowLeft } from 'lucide-react';
+import { Check } from 'lucide-react';
 import React from 'react';
-import { pluginsDetails } from '../../../../../../lib/plugins';
 import { Demo, PluginItem } from '../../../../../../lib/types';
 import { Button } from '../../../../../ui/Button';
 import { Tooltip, TooltipContent, TooltipTrigger } from '../../../../../ui/Tooltip';
+import SidebarHeader from './SidebarHeader';
 
 declare const require: any;
 
@@ -18,8 +17,6 @@ type Props = {
 };
 
 const FeatureSidebar = ({ demo, plugins, setPlugins, onOpen, setShowFeatureLayout }: Props) => {
-	const router = useRouter();
-
 	const selectPlugins = (index: number) => {
 		setPlugins((prevItems: PluginItem[]) =>
 			prevItems.map((item, itemIndex) =>
@@ -30,48 +27,13 @@ const FeatureSidebar = ({ demo, plugins, setPlugins, onOpen, setShowFeatureLayou
 
 	return (
 		<div className="w-[350px] min-w-[350px] flex flex-col bg-[#FAFBFC] border-0 border-r border-solid border-[#E9E9E9] ">
-			<div className="px-6 pt-6">
-				<div className="pb-6 border-0 border-b border-solid border-[#E3E3E3]">
-					<div className="flex justify-between items-center">
-						<h3 className="text-[20px] leading-7 m-0">
-							{__('Features', 'themegrill-demo-importer')}
-						</h3>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<ArrowLeft
-									size={24}
-									color="#909090"
-									strokeWidth={2}
-									onClick={() =>
-										router.navigate({
-											to: '/',
-											search: {
-												search: undefined,
-												builder: undefined,
-												category: undefined,
-											},
-											replace: true,
-										})
-									}
-									className="cursor-pointer"
-								/>
-							</TooltipTrigger>
-							<TooltipContent side="bottom" sideOffset={-4}>
-								{__('Back to Starter Templates', 'themegrill-demo-importer')}
-							</TooltipContent>
-						</Tooltip>
-					</div>
-					<p className="text-[14px] text-[#6B6B6B] leading-[21px] m-0 mt-[6px] ">
-						{__('Select features that you need for your site', 'themegrill-demo-importer')}
-					</p>
-				</div>
-			</div>
+			<SidebarHeader title="Features" subtitle="Select features that you need for your site " />
 			<div className="px-6 pt-6 pb-10 border-0 border-r border-solid border-[#E9E9E9] flex flex-col gap-6 bg-[#FAFBFC] box-border overflow-y-auto flex-1 tg-scrollbar">
 				{plugins.map((item, index) => (
 					<div
-						className={`bg-[#fff] border-2 border-solid rounded-md p-4 ${item.isSelected ? (item.isMandatory ? 'border-[#5182EF]/65 cursor-not-allowed ' : 'border-[#5182EF] cursor-pointer ') : 'border-[#EDEDED] cursor-pointer '}`}
+						className={`bg-[#fff] border-2 border-solid rounded-md p-4 ${item.isSelected ? (item.isMandatory ? 'border-[#5182EF]/65 cursor-not-allowed' : 'border-[#5182EF] cursor-pointer ') : 'border-[#EDEDED] hover:border-[#5182EF] cursor-pointer'}`}
 						key={index}
-						onClick={item.isMandatory ? undefined : () => selectPlugins(index)}
+						{...(!item.isMandatory && { onClick: () => selectPlugins(index) })}
 					>
 						<div className="flex items-center justify-between">
 							<p
@@ -79,38 +41,31 @@ const FeatureSidebar = ({ demo, plugins, setPlugins, onOpen, setShowFeatureLayou
 							>
 								{item.name}
 							</p>
-							{item.isSelected ? (
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width={20} height={20}>
-									<path
-										d="M18.53 10a8.75 8.75 0 1 1-17.5 0 8.75 8.75 0 0 1 17.5 0Z"
-										fill="#5182EF"
-										fillOpacity={item.isMandatory ? 0.65 : 1}
-									/>
-									<path
-										fill="#fff"
-										d="M13.164 6.387a1.118 1.118 0 0 1 1.531 1.626l-6.145 5.78a1.117 1.117 0 0 1-1.556-.023L4.76 11.536a1.117 1.117 0 0 1 1.58-1.58l1.468 1.468 5.356-5.037Z"
-									/>
-								</svg>
-							) : (
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width={20} height={20}>
-									<path
-										d="M18.53 10a8.75 8.75 0 1 1-17.5 0 8.75 8.75 0 0 1 17.5 0Z"
-										fill="#D3D3D3"
-									/>
-									<path
-										fill="#fff"
-										d="M13.164 6.387a1.118 1.118 0 0 1 1.531 1.626l-6.145 5.78a1.117 1.117 0 0 1-1.556-.023L4.76 11.536a1.117 1.117 0 0 1 1.58-1.58l1.468 1.468 5.356-5.037Z"
-									/>
-								</svg>
-							)}
-						</div>
-						{pluginsDetails[item.plugin] && (
-							<p
-								className={`m-0 mt-2 text-[13px] leading-[23px] text-[#545454] ${item.isMandatory ? 'opacity-65' : 'opacity-100'}`}
+							<div
+								className={`relative border-2 border-solid rounded-full w-[14px] h-[14px] text-white ${item.isSelected ? (item.isMandatory ? 'border-[#D3D3D3] bg-[#D3D3D3]' : 'border-[#5182EF] bg-[#5182EF] ') : 'border-[#8c8f94]'}`}
 							>
-								{pluginsDetails[item.plugin]}
-							</p>
-						)}
+								{item.isMandatory ? (
+									<Tooltip>
+										<TooltipTrigger asChild>
+											<Check className="absolute" size={14} strokeWidth={4} />
+										</TooltipTrigger>
+										<TooltipContent side="bottom" sideOffset={-4}>
+											{__(
+												'This plugin is required and cannot be deselected',
+												'themegrill-demo-importer',
+											)}
+										</TooltipContent>
+									</Tooltip>
+								) : (
+									<Check className="absolute" size={14} strokeWidth={4} />
+								)}
+							</div>
+						</div>
+						<p
+							className={`m-0 mt-2 text-[13px] leading-[23px] text-[#545454] ${item.isMandatory ? 'opacity-65' : 'opacity-100'}`}
+						>
+							{item.description}
+						</p>
 					</div>
 				))}
 			</div>
